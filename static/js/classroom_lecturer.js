061b6a329a5cef24e4eae8d9113bd96950f029d1
naidukarthi2193 <naidukarthi2193@gmail.com>
2020-07-16 17:10:00 +0000

    temp socket

--- static/js/classroom_lecturer.js
+++ static/js/classroom_lecturer.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
     // });
   });
   document.addEventListener('DOMContentLoaded', function(){
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/student');
+    var socket = io.connect('https://' + document.domain + ':' + location.port + '/student');
 
     let character = ['a','b','c','d','e','f', 'g', 'h','i','j','k','l','m','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w'];
   
--- static/js/classroom_student.js
+++ static/js/classroom_student.js
@@ -1,7 +1,7 @@
 // Client Side Javascript to receive numbers.
 $(document).ready(function(){
   // start up the SocketIO connection to the server - the namespace 'test' is also included here if necessary
-  var socket = io.connect('http://' + document.domain + ':' + location.port + '/student');
+  var socket = io.connect('https://' + document.domain + ':' + location.port + '/student');
   // this is a callback that triggers when the "my response" event is emitted by the server.
   // socket.on('my response', function(msg) { 
   //   console.log(msg.data);
